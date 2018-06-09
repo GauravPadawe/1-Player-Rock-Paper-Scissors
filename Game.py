7897c904fe5063d7bf5f720297c0b0c06e518c7f
Gaurav Padawe <39410780+GauravPadawe@users.noreply.github.com>
2018-06-09 14:50:56 +0530

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,13 +1,13 @@
-import sys
+import sys                                                                   # Importing required packages
 import random
 import time
 
-def game(user, choice):
-	choice = str(input(user + ", What's your choice?: "))
-	opt = ["Rock", "Paper", "Scissors"]
-	bot = random.choice(opt)
-	time.sleep(1)
-	if choice == "Rock" and bot == "Scissors":
+def game(user, choice):                              # Defining a function which will accept 2 inputs from user as (user, choice)
+	choice = str(input(user + ", What's your choice?: "))      # Choice will accept value Rock/ Paper/ Scissors
+	opt = ["Rock", "Paper", "Scissors"]                        # opt is a list of values
+	bot = random.choice(opt)                                   # bot will contain random values from opt
+	time.sleep(1)                                              # Delayed output by 1 second
+	if choice == "Rock" and bot == "Scissors":                        # If else statements nested within
 		print (user + ", you WON !! as bot chose scissors")
 	elif choice == "Rock" and bot == "Paper":
 		print (user + ", you LOSE !! as bot chose paper")
@@ -21,22 +21,24 @@ def game(user, choice):
 		print (user + ", you LOSE !! as bot chose rock")
 	elif choice == bot:
 		print ("It's a TIE !!")
-	elif choice not in opt:
+	elif choice not in opt:                                     # if choice of user is not in opt then return invalid input
 		print ("Oh NO !! Invalid input. Choose only from <Rock/Paper/Scissors>")
 		
-def start():
+def start():                                                       # Defining a function start to make game little more interactive
 	while True:
-		user = str(input("What's your name?: "))
-		choice = ""
-		print (game(user, choice))
-		while True:
-			x = input(user + " , Do you want to play again ? Type Y/N: ")
-			if x == "Y":
+		user = str(input("What's your name?: "))           # User's name will be contained here
+		choice = ""                                        # Passing choice empty
+		print (game(user, choice))                         # game() will run in this block
+		while True:                                        # While loop so user will be propted to play again
+			x = input(user + " , Do you want to play again ? Type Y/N: ")  # Accepting value from user as Yes/No
+			if x == "Y":                               # if value is Y restart the game with same user
 				print (game(user, choice))
-			elif x == "N":
+			elif x == "N":                             # if value is N then exit the game
 				print ("Goodbye " + user)
 				sys.exit()
-			else:
+			else:                                      # if user passes value other than Y/N prompt him with Invalid Input
 				print ("Invalid Input")
 				
-print (start())
+print (start())                                                    # Run the Game
+
+# CODED BY - GAURAV PADAWE
